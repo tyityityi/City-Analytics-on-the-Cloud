f26c69ddc9cb7e2bb1f4abc12349dad107842511
ziyanzhang2 <80999016+ziyanzhang2@users.noreply.github.com>
2021-05-25 20:02:39 +0800

    Update aurin_crime.py

--- Data/DataAnalysis/aurin_crime.py
+++ Data/DataAnalysis/aurin_crime.py
@@ -1,3 +1,11 @@
+# COMP90024     Group 66
+# Ziyang Zhang 	1139552
+# Yanjun Ma     1184516
+# Tianyi Zheng 	1024493
+# Yining Ding 	874213
+# Zixin Zhang 	1087336
+# Description   This document is used to save the crime data from abs.gov.au to the database in CouchDB
+
 import pandas as pd
 data=pd.read_excel('tmpB2ZP9eAFP---Crime-incidents-data.xls')
 column=data[4:5]
