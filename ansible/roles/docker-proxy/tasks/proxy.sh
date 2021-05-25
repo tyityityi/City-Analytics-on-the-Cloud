f19b264c1c416ee4425c7bee5f3aab6d444a56b8
AshleyZ125 <69858908+AshleyZ125@users.noreply.github.com>
2021-05-26 00:28:40 +0800

    Delete ansible directory

--- ansible/roles/docker-proxy/tasks/proxy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo mkdir -p /etc/systemd/system/docker.service.d
-sudo cp http-proxy.conf /etc/systemd/system/docker.service.d/
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-
